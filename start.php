640ff3c93727af675a5b82f75e5447f8c2ecdfc9
Matthias Gatto <uso.cosmo.ray@gmail.com>
2022-10-08 00:56:37 +0200

    beerware lisence
    Signed-off-by: Matthias Gatto <uso.cosmo.ray@gmail.com>

--- start.php
+++ start.php
@@ -1,17 +1,13 @@
 <?php
-//           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-//                   Version 2, December 2004
-//
-// Copyright (C) 2022 Matthias Gatto <uso.cosmo.ray@gmail.com>
-//
-// Everyone is permitted to copy and distribute verbatim or modified
-// copies of this license document, and changing it is allowed as long
-// as the name is changed.
-//
-//            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-//   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-//
-//  0. You just DO WHAT THE FUCK YOU WANT TO.
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * <matthias.gatto@protonmail.com> wrote this file.
+ * As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Matthias Gatto
+ * ----------------------------------------------------------------------------
+ */
 
 $MAP_W = 160;
 $MAP_H = 160;
